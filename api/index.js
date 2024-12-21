59b309e622a8fc61f003ce7219aa8dd6d0b225b2
new <uday45855@gmail.com>
2024-12-21 17:44:15 +0530

    modified

--- api/index.js
+++ api/index.js
@@ -1,64 +1,68 @@
-require('dotenv').config()
-const sql=require('mysql')
-const express=require('express')
-const app=express()
-const cors =require('cors')
-app.use(express.json())
-app.use(cors()) 
+// require('dotenv').config()
+// const sql=require('mysql')
+// const express=require('express')
+// const app=express()
+// const cors =require('cors')
+// app.use(express.json())
+// app.use(cors()) 
 
-const pool =sql.createPool({
-    host:process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database:process.env.DB_DBNAME,
-    port:3306,
-    waitForConnections: true,
-    connectionLimit: 5,
+// const pool =sql.createPool({
+//     host:process.env.DB_HOST,
+//     user: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database:process.env.DB_DBNAME,
+//     port:3306,
+//     waitForConnections: true,
+//     connectionLimit: 5,
     
-});
+// });
 
-app.get('/schools',(req,res)=>{
-    pool.getConnection((err,connection)=>{
-        if(err){
-            connection.release();
-            return res.json(err)
-        }
+// app.get('/schools',(req,res)=>{
+//     pool.getConnection((err,connection)=>{
+//         if(err){
+//             connection.release();
+//             return res.json(err)
+//         }
             
-        else {
-            connection.query("select * from schools",(err,data)=>{
+//         else {
+//             connection.query("select * from schools",(err,data)=>{
 
-        connection.release();
-                console.log(data);
-        return res.json(data);
-            })
-        }
-    });
+//         connection.release();
+//                 console.log(data);
+//         return res.json(data);
+//             })
+//         }
+//     });
     
-})
+// })
 
-const storage=multer.diskStorage({
-    destination:(req,file,cb)=>{
-            cb(null,'/temp')},
-    filename:(req,file,cb)=>{
+// const storage=multer.diskStorage({
+//     destination:(req,file,cb)=>{
+//             cb(null,'/temp')},
+//     filename:(req,file,cb)=>{
         
-cb(null,file.fieldname+""+path.extname(file.originalname))
-//cb(null, 'image.png')
-}})
+// cb(null,file.fieldname+""+path.extname(file.originalname))
+// //cb(null, 'image.png')
+// }})
 
-const upload=multer({
-    storage:storage})
+// const upload=multer({
+//     storage:storage})
 
-app.post('/upload',upload.single('image'),(req,res)=>{
-    console.log(JSON.stringify(req.body));
-    try {
-        let file=req.file;
-        var base64String= fs.readFileSync(file.path, 'base64');
+// app.post('/upload',upload.single('image'),(req,res)=>{
+//     console.log(JSON.stringify(req.body));
+//     try {
+//         let file=req.file;
+//         var base64String= fs.readFileSync(file.path, 'base64');
             
-        res.send(req.body);   
-    } catch (error) {
+//         res.send(req.body);   
+//     } catch (error) {
         
-    }
+//     }
     
     
-})
-app.listen(5000,console.log('server is running'));
+// })
+// app.listen(5000,console.log('server is running'));
+
+module.exports=(req,res)=>{
+    res.status(200).json({message:"hello !!!"});
+};
